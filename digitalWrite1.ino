26b334d6521ef4ecb2b94c69613ae89eb89c796f
run0122 <run0122@naver.com>
2023-03-30 11:37:57 +0900

    밝기가 증가하는 코드

--- digitalWrite1.ino
+++ digitalWrite1.ino
@@ -3,15 +3,21 @@ const int LED = 13;
 void setup() {
   pinMode(LED, OUTPUT);
   // digitalWrite(LED, HIGH);
-  digitalWrite(LED, LOW);
+  // digitalWrite(LED, LOW);
 }
 
 void loop() {
-  for (int t_high=0; t_high<=10; t_high++){
-    digitalWrite(LED, HIGH);
-    delay(t_high);
-    digitalWrite(LED, LOW);
-    delay(10-t_high);
+  for (int t_high = 0; t_high <= 10; t_high++) {
+    int cnt = 0;
+    while (true) {
+      digitalWrite(LED, HIGH);
+      delay(t_high);
+      digitalWrite(LED, LOW);
+      delay(10 - t_high);
+
+      cnt++;
+      if (cnt == 10) break;
+    }
   }
   // digitalWrite(LED, HIGH);
   // delay(1);
